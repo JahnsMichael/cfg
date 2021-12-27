f33328527d65450feeb7b388bdfb8a3e8232b1f1
Jahns Michael <michael.jf.jm@gmail.com>
2021-12-27 15:40:11 +0700

    matches libreoffice

--- .config/qtile/lib/groups/__init__.py
+++ .config/qtile/lib/groups/__init__.py
@@ -2,12 +2,7 @@ from libqtile.config import (
     Group,
     Match,
     ScratchPad,
-    DropDown
-)
-from lib.const.apps import ( 
-  WHATSAPP,
-  LINE,
-  WEB
+    DropDown,
 )
 
 GROUPS = [
@@ -17,7 +12,9 @@ GROUPS = [
     Group("4"),
     Group("5"),
     Group("6"),
-    Group("7"),
+    Group("7", matches=[
+        Match(wm_class="libreoffice"),
+    ]),
     Group("8", matches=[
         Match(wm_class="zoom"),
     ]),
@@ -25,16 +22,16 @@ GROUPS = [
         Match(wm_class="web.whatsapp.com"),  # Whatsapp Web
         Match(wm_class="ophjlpahpchlmihnnnihgmmeilfjmjjc__index.html"), # LINE Browser Extension
     ]),
-    Group("0")
+    Group("0", matches=[
+        Match(wm_class="discord")
+    ])
 ]
-
-cmd = "kitty"
 
 GROUPS.append(
     ScratchPad("scratchpad", [
         DropDown(
             "term",
-            cmd,
+            "alacritty",
             opacity=1.0,
             x=0.05, y=0.05,
             width=0.9, height=0.9,
